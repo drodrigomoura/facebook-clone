94412777cf0851d0f18cf54ff408e7e0c1c34487
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2020-12-08 23:25:29 -0300

    7. Spiking Out the First Post Test

--- tests/Feature/PostToTimelineTest.php
+++ tests/Feature/PostToTimelineTest.php
@@ -2,21 +2,32 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PostToTimelineTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function a_user_can_create_a_text_post()
+    {
+        $this->withoutExceptionHandling();
+        $this->actingAs($user = factory(User::class)->create(), 'api');
+
+        $response = $this->post('/api/posts', [
+            'data' => [
+                'type' => 'posts',
+                'attributes' => [
+                    'body' => 'Testing Body'
+                ]
+            ]
+        ]);
+
+        $post = \App\Post::first();
+
+        $response->assertStatus(201);
     }
 }
